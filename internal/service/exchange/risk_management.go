00ae203a18ec9e096bd7ae63171eb9ef6f175a65
fifak15 <ganarena6@gmail.com>
2025-04-22 17:49:24 +0300

    делаю более гибкую логику,исправляю пробелмы баров брал хрень

--- internal/service/exchange/risk_management.go
+++ internal/service/exchange/risk_management.go
@@ -1,10 +1,7 @@
 package exchange
 
 import (
-	"bybit-bot/internal/model"
 	"github.com/thrasher-corp/gocryptotrader/exchanges/order"
-	"log"
-	"math"
 )
 
 // RiskManagement формирует RiskManagementModes на основе переданных параметров стоп‑лосса и тейк‑профита.
@@ -31,32 +28,4 @@ func RiskManagement(stopLoss, takeProfit float64) order.RiskManagementModes {
 		}
 	}
 	return rm
-}
-
-func CalculateOrderPrices(midPrice float64, decision model.FacadeResponse, commission float64) (float64, float64) {
-	const spreadPercent = 0.0001
-	adjustmentRaw := 0.0001 * (decision.Buy - decision.Sell)
-	adjustment := math.Max(math.Min(adjustmentRaw, 0.001), -0.001)
-
-	rawBuyPrice := midPrice * (1 - spreadPercent - adjustment)
-	rawSellPrice := midPrice * (1 + spreadPercent + adjustment)
-
-	buyPrice := rawBuyPrice * (1 + commission)
-	sellPrice := rawSellPrice * (1 - commission)
-	log.Printf("Raw BUY: %.5f, Raw SELL: %.5f, с комиссией: BUY=%.5f, SELL=%.5f", rawBuyPrice, rawSellPrice, buyPrice, sellPrice)
-	return buyPrice, sellPrice
-}
-
-func CalculateStopLoss(price float64, isBuy bool, stopLossPercent float64) float64 {
-	if isBuy {
-		return price * (1 - stopLossPercent)
-	}
-	return price * (1 + stopLossPercent)
-}
-
-func CalculateTakeProfit(orderPrice float64, isBuy bool, profitPercent float64) float64 {
-	if isBuy {
-		return orderPrice * (1 + profitPercent)
-	}
-	return orderPrice * (1 - profitPercent)
 }
